3aad8c6631f6a7da99b5ce7aab994188d081dc95
Eze <eze.largaespada@gmail.com>
2020-02-02 11:44:23 -0500

    update

--- script.js
+++ script.js
@@ -1,18 +1,97 @@
 var task=new Date();
 var hr=task.getHours();
 //time 24 hr
+
+//present time red outline
 function changeColor(){
   if($("#saveInfo").attr("time")==hr){
-    $("#saveInfo").css("background-color","red")
+    $("#saveInfo").css("border-color","red")
+  }
+  if($("#saveInfo1").attr("time")==hr){
+    $("#saveInfo1").css("border-color","red")
+  }
+  if($("#saveInfo2").attr("time")==hr){
+    $("#saveInfo2").css("border-color","red")
+  }
+  if($("#saveInfo3").attr("time")==hr){
+    $("#saveInfo3").css("border-color","red")
+  }
+  if($("#saveInfo4").attr("time")==hr){
+    $("#saveInfo4").css("border-color","red")
+  }
+  if($("#saveInfo5").attr("time")==hr){
+    $("#saveInfo5").css("border-color","red")
+  }
+  if($("#saveInfo6").attr("time")==hr){
+    $("#saveInfo6").css("border-color","red")
+  }
+  if($("#saveInfo7").attr("time")==hr){
+    $("#saveInfo7").css("border-color","red")
+  }
+  if($("#saveInfo8").attr("time")==hr){
+    $("#saveInfo8").css("border-color","red")
   }
 
-if($("#saveInfo").attr("time")<hr){
+
+
+//past time background gray
+  if($("#saveInfo").attr("time")<hr){
+  $("#saveInfo").css("background-color","gray")
+}
+if($("#saveInfo1").attr("time")<hr){
+  $("#saveInfo1").css("background-color","gray")
+}
+if($("#saveInfo2").attr("time")<hr){
+  $("#saveInfo2").css("background-color","gray")
+}
+if($("#saveInfo3").attr("time")<hr){
+  $("#saveInfo3").css("background-color","gray")
+}
+if($("#saveInfo4").attr("time")<hr){
+  $("#saveInfo4").css("background-color","gray")
+}
+if($("#saveInfo5").attr("time")<hr){
+  $("#saveInfo5").css("background-color","gray")
+}
+if($("#saveInfo6").attr("time")<hr){
+  $("#saveInfo6").css("background-color","gray")
+}
+if($("#saveInfo7").attr("time")<hr){
+  $("#saveInfo7").css("background-color","gray")
+}
+if($("#saveInfo8").attr("time")<hr){
   $("#saveInfo").css("background-color","gray")
 }
 
+//future time outline green
 if($("#saveInfo").attr("time")>hr){
-  $("#saveInfo").css("background-color","green")
+  $("#saveInfo").css("border-color","green")
 }
+if($("#saveInfo1").attr("time")>hr){
+  $("#saveInfo1").css("border-color","green")
+}
+if($("#saveInfo2").attr("time")>hr){
+  $("#saveInfo2").css("border-color","green")
+}
+if($("#saveInfo3").attr("time")>hr){
+  $("#saveInfo3").css("border-color","green")
+}
+if($("#saveInfo4").attr("time")>hr){
+  $("#saveInfo4").css("border-color","green")
+}
+if($("#saveInfo5").attr("time")>hr){
+  $("#saveInfo5").css("border-color","green")
+}
+if($("#saveInfo6").attr("time")>hr){
+  $("#saveInfo6").css("border-color","green")
+}
+if($("#saveInfo7").attr("time")>hr){
+  $("#saveInfo7").css("border-color","green")
+}
+if($("#saveInfo8").attr("time")>hr){
+  $("#saveInfo8").css("border-color","green")
+}
+
 }
 
 
@@ -20,7 +99,7 @@ if($("#saveInfo").attr("time")>hr){
 
 
 
-
+//date n time
 function timeclock(){
       setInterval(function(){
           var time = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -29,7 +108,7 @@ function timeclock(){
   }
 
 
-
+// save unput user
 function saveDate(){
   localStorage.setItem("9",$("#saveInfo").val())
   localStorage.setItem("10",$("#saveInfo1").val())
@@ -42,7 +121,7 @@ function saveDate(){
   localStorage.setItem("5",$("#saveInfo8").val())
 }
 
-
+//local storage 
 function userInfo(){
 timeclock()
 changeColor()
